4dae01c6a7636a8f80c80014a9a7b503b89a8f88
freestyletear <freestyletear@gmail.com>
2021-09-02 16:36:55 +0300

    conduct unittest for the user class

--- user_test.py
+++ user_test.py
@@ -1,2 +1,33 @@
-import unittest 
-import pyperclip  
+import unittest  # Importing the unittest module
+import pyperclip  # helps in copy and paste functions
+
+from user import User, Credential
+
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that defines test cases for the users class behaviours
+    Args:
+        unittest.TestCase: Testcase class that helps create test cases
+    '''
+
+    def setUp(self):
+        '''
+        Function to help create user account details before each test
+        '''
+        self.new_user = User('Tom', 'Chege', 'angry')
+
+    def test_init_(self):
+        '''
+        Test to check creation of new user instance
+        '''
+        self.assertEqual(self.new_user.first_name, 'Keith')
+        self.assertEqual(self.new_user.last_name, 'James')
+        self.assertEqual(self.new_user.password, 'freestyletear')
+
+    def test_save_user(self):
+        '''
+        Test to check if New user information is saved into the users_list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.users_list), 1)
