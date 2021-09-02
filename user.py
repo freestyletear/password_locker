61039797195d30b0d5e3c43dbf76ee9e4a090fbb
freestyletear <freestyletear@gmail.com>
2021-09-02 15:35:04 +0300

    add class method for desplaying the credential list and a password generator

--- user.py
+++ user.py
@@ -42,3 +42,37 @@ class Credential:
             if (user.first_name == first_name and user.password == password):
                 active_user = user.first_name
         return active_user
+
+    def __init__(self, user_name, social_media, account_name, password):
+        '''
+        Method defining the properties each object will hold
+        '''
+
+        self.user_name = user_name
+        self.social_media = social_media
+        self.account_name = account_name
+        self.password = password
+
+    def save_credentials(self):
+        '''
+        Function to save new user credentials
+        '''
+        Credential.credentials_list.append(self)
+
+    def generate_password():
+        '''
+        Function to generate random passwords for social media accounts
+        '''
+        pwchar = string.printable
+        length = int(input('Enter password length desired: '))
+        generate_password = '' #It was gen_pwd = ''
+        for pwchar in range(length): # I have added pw in the pwchar
+            generate_password += random.choice(pwchar)
+        return generate_password
+
+    @classmethod
+    def display_credentials(cls):
+        '''
+        Class method to display the list of saved credentials
+        '''
+        return cls.credentials_list
